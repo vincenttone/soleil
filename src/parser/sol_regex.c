17e1d7a2f99112846c5247be41c8137d6e892cff
vincent <tongwensong@gmail.com>
2017-05-09 22:55:50 +0800

    regex

---
+++ src/parser/sol_regex.c
@@ -0,0 +1,26 @@
+#include "sol_regex.h"
+
+SolRegexEngine* solRegexEngine_new(SolRegexEncoding *ec)
+{
+	SolRegexEngine *re = sol_alloc(sizeof(SolRegexEngine));
+	if (re == NULL) {
+		return NULL;
+	}
+	re->g = solPatternStateGen_new();
+	if (re->g == NULL) {
+		solRegexEngine_free(re);
+		return NULL;
+	}
+	re->ec = ec;
+	return re;
+}
+
+void solRegexEngine_free(SolRegexEngine *re)
+{
+	if (re->g) {
+		solPatternStateGen_free(re->g);
+	}
+	if (re) {
+		sol_free(re);
+	}
+}
--- src/parser/sol_regex.h
+++ src/parser/sol_regex.h
@@ -1,17 +1,22 @@
 #ifndef _SOL_REGEX_H_
 #define _SOL_REGEX_H_ 1
 
+#include "sol_common.h"
+#include "sol_pattern.h"
+
 typedef enum _SolRegexEncoding {
 	SolRegexEncode_ascii = 1,
 	SolRegexEncode_utf8 = 2,
 } SolRegexEncoding;
 
 typedef struct _SolRegexEngine {
-	SolRegexEncoding e;
+	SolPatternStateGen *g;
+	SolRegexEncoding ec;
 } SolRegexEngine;
 
-typedef struct _SolRegex {
-	
-} SolRegex;
+SolRegexEngine* solRegexEngine_new(SolRegexEncoding);
+void solRegexEngine_free(SolRegexEngine*);
+
+SolPattern* solRegexEngine_conv_pattern(SolRegexEngine*, char*);
 
 #endif
