ade1afe9384df445b80ea1c80222db0dc1dc6c0c
vincent <tongwensong@gmail.com>
2017-08-01 20:07:45 +0800

    spare matrix

---
+++ src/basekit/sol_sparse_matrix.c
@@ -0,0 +1,22 @@
+#include "sol_spare_matrix.h"
+
+SolSparseMatrix* solSparseMatrix_new(size_t h, size_t w, enum _SolSparseMatrixRecordType t)
+{
+    SolSparseMatrix *s = sol_calloc(1, sizeof(SolSparseMatrix));
+    s->h = h;
+    s->w = w;
+    s->t = t;
+    s->s = h + w;
+    s->r = sol_calloc(1, sizeof(SolSparseMatrixRecord) * s->s);
+    s->offset = sol_calloc(1, sizeof(size_t) * s->s);
+    s->col = sol_calloc(1, sizeof(size_t) * s->s);
+    return s;
+}
+
+void solSparseMatrix_free(SolSparseMatrix *s)
+{
+    sol_free(s->r);
+    sol_free(s->offset);
+    sol_free(s->col);
+    sol_free(s);
+}
---
+++ src/basekit/sol_sparse_matrix.h
@@ -0,0 +1,33 @@
+#ifndef _SOL_SPARSE_MAXTRIX_H_
+#define _SOL_SPARSE_MAXTRIX_H_ 1
+
+#include "sol_common.h"
+
+enum _SolSparseMatrixRecordType {
+    SolSparseMatrixRecordType_Char = 1,
+    SolSparseMatrixRecordType_Int,
+    SolSparseMatrixRecordType_Ptr,
+};
+
+typedef union {
+    char rc;
+    int ri;
+    void *rp;
+} SolSparseMatrixRecord;
+
+typedef struct _SolSparseMatrix {
+    size_t h; // height
+    size_t w; // weight
+    size_t s; // size
+    size_t c; // count
+    enum _SolSparseMatrixRecordType t; // record type
+    SolSparseMatrixRecord *r; // records
+    size_t *col; // column
+    size_t *offset; // offset
+} SolSparseMatrix;
+
+SolSparseMatrix* solSparseMatrix_new(size_t, size_t, enum _SolSparseMatrixRecordType);
+void solSparseMatrix_free(SolSparseMatrix*);
+
+
+#endif
