4042476b1b32d28250019a81376caf93040b4520
vincent <tongwensong@gmail.com>
2017-08-07 12:01:34 +0800

    sparse matrix set & get

--- src/basekit/sol_sparse_matrix.c
+++ src/basekit/sol_sparse_matrix.c
@@ -1,27 +1,87 @@
 #include "sol_spare_matrix.h"
 
-SolSparseMatrix* solSparseMatrix_new(size_t h, size_t w, enum _SolSparseMatrixRecordType t)
+SolSparseMatrix* solSparseMatrix_new(size_t row_size, size_t col_size, enum _SolSparseMatrixRecordType t)
 {
-    SolSparseMatrix *s = sol_calloc(1, sizeof(SolSparseMatrix));
-    s->h = h;
-    s->w = w;
-    s->t = t;
-    s->s = h + w;
-    s->r = sol_calloc(1, sizeof(SolSparseMatrixRecord) * s->s);
-    s->offset = sol_calloc(1, sizeof(size_t) * s->s);
-    s->col = sol_calloc(1, sizeof(size_t) * s->s);
-    return s;
+    SolSparseMatrix *m = sol_calloc(1, sizeof(SolSparseMatrix));
+    if (m == NULL) {
+        return NULL;
+    }
+    solSparseMatrix_set_row_size(m, row_size);
+    solSparseMatrix_set_col_size(m, col_size);
+    solSparseMatrix_set_record_type(m, t);
+    solSparseMatrix_set_size(m, row_size + col_size);
+    solSparseMatrix_set_records(m, sol_calloc(1, sizeof(SolSparseMatrixRecord) * (solSparseMatrix_size(m))));
+    solSparseMatrix_set_columns(m, sol_calloc(1, sizeof(size_t) * (solSparseMatrix_size(m))));
+    solSparseMatrix_set_offsets(m, sol_calloc(1, sizeof(size_t) * (solSparseMatrix_size(m))));
+    if (solSparseMatrix_records(m) == NULL
+        ||solSparseMatrix_columns(m) == NULL
+        || solSparseMatrix_offsets(m) == NULL
+        ) {
+        solSparseMatrix_free(m);
+        return NULL;
+    }
+    return m;
 }
 
-void solSparseMatrix_free(SolSparseMatrix *s)
+void solSparseMatrix_free(SolSparseMatrix *m)
 {
-    sol_free(s->r);
-    sol_free(s->offset);
-    sol_free(s->col);
-    sol_free(s);
+    if (solSparseMatrix_records(m)) sol_free(solSparseMatrix_records(m));
+    if (solSparseMatrix_columns(m)) sol_free(solSparseMatrix_columns(m));
+    if (solSparseMatrix_offsets(m)) sol_free(solSparseMatrix_offsets(m));
+    sol_free(m);
 }
 
-int solSparseMatrix_add_char(SolSparseMatrix *m, size_t col, size_t row, SolSparseMatrixRecord r)
+int solSparseMatrix_set(SolSparseMatrix *m, size_t row, size_t col, SolSparseMatrixRecord r)
 {
-    
+    if (row > solSparseMatrix_row_size(m) - 1) return -1;
+    if (col > solSparseMatrix_col_size(m) - 1) return -1;
+    size_t *o1 = solSparseMatrix_offsets(m) + row;
+    size_t *o2 = o1 + 1;
+    size_t o;
+    int ac = 1; // prepend
+    foreach (o = *o1; o < *o2; o++) {
+        if (col == *(size_t*)(solSparseMatrix_columns(m) + o)) {
+            ac = 2; // swap
+            break;
+        } else if (col < *(size_t*)(solSparseMatrix_columns(m) + o)) {
+            continue;
+        } else {
+            break;
+        }
+    }
+    if (ac == 2) { // swap record
+        SolSparseMatrixRecord *r2s = (solSparseMatrix_records(m) + *(size_t*)o);
+        // todo: free pre record
+        *r2s = r;
+        return 0;
+    }
+    size_t s = solSparseMatrix_size(m) - 1;
+    size_t *c2s;
+    SolSparseMatrixRecord *r2s;
+    for (s; s > *o; s--) {
+        c2s = solSparseMatrix_columns(m) + s + 1;
+        *c2s = *(solSparseMatrix_columns(m) + s);
+        r2s = solSparseMatrix_records(m) + s + 1;
+        *r2s = *(solSparseMatrix_records(m) + s);
+    }
+    c2s = solSparseMatrix_columns(m) + o + 1;
+    *c2s = col;
+    r2s = solSparseMatrix_records(m) + o + 1;
+    *r2s = r;
+    return 0;
+}
+
+SolSparseMatrixRecord* solSparseMatrix_get(SolSparseMatrix *m, size_t row, size_t col)
+{
+    if (row > solSparseMatrix_row_size(m) - 1) return -1;
+    if (col > solSparseMatrix_col_size(m) - 1) return -1;
+    size_t *o1 = solSparseMatrix_offsets(m) + row;
+    size_t *o2 = o1 + 1;
+    size_t o;
+    foreach (o = *o1; o < *o2; o++) {
+        if (col == *(size_t*)(solSparseMatrix_columns(m) + o)) {
+            return (SolSparseMatrixRecord*)(solSparseMatrix_records(m) + o);
+        }
+    }
+    return NULL;
 }
--- src/basekit/sol_sparse_matrix.h
+++ src/basekit/sol_sparse_matrix.h
@@ -16,18 +16,40 @@ typedef union {
 } SolSparseMatrixRecord;
 
 typedef struct _SolSparseMatrix {
-    size_t h; // height
-    size_t w; // weight
+    size_t cs; // col size
+    size_t rs; // row size
     size_t s; // size
     size_t c; // count
-    enum _SolSparseMatrixRecordType t; // record type
+    enum _SolSparseMatrixRecordType rt; // record type
     SolSparseMatrixRecord *r; // records
-    size_t *col; // column
-    size_t *offset; // offset
+    size_t *cols; // column
+    size_t *offsets; // offset
 } SolSparseMatrix;
 
 SolSparseMatrix* solSparseMatrix_new(size_t, size_t, enum _SolSparseMatrixRecordType);
 void solSparseMatrix_free(SolSparseMatrix*);
-int solSparseMatrix_add_char(SolSparseMatrix*, size_t, size_t, SolSparseMatrixRecord);
+int solSparseMatrix_set(SolSparseMatrix*, size_t, size_t, SolSparseMatrixRecord);
+SolSparseMatrixRecord* solSparseMatrix_get(SolSparseMatrix*, size_t, size_t);
+
+#define solSparseMatrix_set_size(m, size) m->s = size
+#define solSparseMatrix_size(m) m->s
+
+#define solSparseMatrix_set_row_size(m, s) m->rs = s
+#define solSparseMatrix_row_size(m) m->rs
+
+#define solSparseMatrix_set_col_size(m, s) m->cs = s
+#define solSparseMatrix_col_size(m) m->cs
+
+#define solSparseMatrix_set_record_type(m, t) m->rt = t
+#define solSparseMatrix_record_type(m) m->rt
+
+#define solSparseMatrix_set_records(m, records) m->r = records
+#define solSparseMatrix_records(m) m->r
+
+#define solSparseMatrix_set_columns(m, c) m->cols = c
+#define solSparseMatrix_columns(m) m->cols
+
+#define solSparseMatrix_set_offsets(m, o) m->offsets = o
+#define solSparseMatrix_offsets(m) m->offsets
 
 #endif
