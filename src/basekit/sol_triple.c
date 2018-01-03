169b016d57853c327d8f33d52b2bfcae3fc39a9e
vincent.tone <tongwensong@gmail.com>
2018-01-03 12:22:31 +0000

    triple

--- src/basekit/sol_triple.c
+++ src/basekit/sol_triple.c
@@ -7,169 +7,61 @@ SolTriple* solTriple_new()
     if (t == NULL) {
         return NULL;
     }
-    if (solTriple_set_size(t, SOL_TRIPLE_INIT_SIZE) != 0) {
-        return NULL;
-    }
+	t->h1 = solHash_new();
+	t->h2 = solHash_new();
+	t->tmp_k = sol_alloc(sizeof(SolTripleK));
     return t;
-}
-
-int solTriple_set_size(SolTriple *t, size_t s)
-{
-    SolTripleRecord *r = sol_calloc(1, sizeof(SolTripleRecord) * s);
-    if (r == NULL) {
-        return -1;
-    }
-    solTriple_set_records(t, r);
-    solTriple_set_size(t, s);
-    solTriple_update_mask(t);
-    return 0;
 }
 
 void solTriple_free(SolTriple *t)
 {
-    if (solTriple_record(t)) {
-        solTripleRecord_free(solTriple_record(t));
-    }
+	if (t->h1) {
+		sol_free(t->h1);
+	}
+	if (t->h2) {
+		sol_free(t->h2);
+	}
     sol_free(t);
 }
 
-int solTripleRecord_register(SolTriple *t, SolTripleRecord *r)
+int solTriple_put(SolTriple *t, void *v1, void *v2, void *v3)
 {
-    //solHash_put(solTripleRecord_v1(r1), r);
+	if (t == NULL) {
+		return -1;
+	}
+	SolTripleK *k = sol_alloc(sizeof(SolTripleK));
+	k->v1 = v1;
+	k->v2 = v2;
+	SolList *l = solHash_get(t->h1, v1);
+	if (l == NULL) {
+		l = solList_new();
+		if (solHash_put(t->h1, v1, l) != 0) {
+			return 1;
+		}
+	}
+	if (solList_add(l, k) == NULL) {
+		return 2;
+	}
+	if (solHash_put(t->h2, k, v3) != 0) {
+		return 3;
+	}
+	return 0;
 }
 
-int solTriple_add(SolTriple *t, void *v1, void *v2, void *v3)
+SolList* solTriple_get_by_v1(SolTriple *t, void *v1)
 {
-    SolTripleRecord * r = solTriple_record1_of_val(t, v1, v2);
-    if (solTripleRecord_v1(r) == NULL) {
-        goto up_record;
-    }
-    if (solTriple_v1_match(solTripleRecord_v1(r), v1) == 0
-        && solTriple_v2_match(solTripleRecord_v2(r), v2) == 0
-        ) {
-        solTripleRecord_set_v3(r, v3);
-        return 0;
-    }
-    r = solTriple_record2_of_val(t, v1, v2);
-    if (solTripleRecord_v1(r) == NULL) {
-        goto up_record;
-    }
-    if (solTriple_v1_match(solTripleRecord_v1(r), v1) == 0
-        && solTriple_v2_match(solTripleRecord_v2(r), v2) == 0
-        ) {
-        solTripleRecord_set_v3(r, v3);
-        return 0;
-    }
-    return solTriple_adjust_and_put(t, v1, v2, v3);
-up_record:
-    if (solTripleRecord_register(t, r) != 0) {
-        solTripleRecord_set_v1(r, NULL);
-        return -1;
-    }
-    solTripleRecord_set_v1(r, v1);
-    solTripleRecord_set_v2(r, v2);
-    solTripleRecord_set_v3(r, v3);
-    solTriple_count_incr(t);
-    return 0;
+	if (t == NULL) {
+		return -1;
+	}
+	return solHash_get(t->h1, v1);
 }
 
-int solTriple_adjust_and_put(SolTriple *t, void *v1, void *v2, void *v3)
+void* solTriple_get(SolTriple *t, void *v1, void *v2)
 {
-    SolTripleRecord *r;
-    SolTripleRecord rs;
-    size_t i;
-    for (i = 0; i < solTriple_size(t) * 2; i++) {
-        r = solTriple_record1_of_val(t, v1, v2);
-        if (solTripleRecord_v1(r) == NULL) {
-            goto up_record;
-        }
-        solTriple_record_switch(r, &rs);
-        r = solTriple_record2_of_val(t, v1, v2);
-        if (solTripleRecord_v1(r) == NULL) {
-            goto up_record;
-        }
-        solTriple_record_switch(r, &rs);
-    }
-    if (solTriple_is_resizing(t)) {
-        return -2;
-    }
-    if (solTriple_grow(t) != 0) {
-        return -3;
-    }
-    return solTriple_add(t, v1, v2, v3);
-up_record:
-    if (solTripleRecord_register(t, r) != 0) {
-        solTripleRecord_set_v1(r, NULL);
-        return -1;
-    }
-    solTripleRecord_set_v1(r, v1);
-    solTripleRecord_set_v2(r, v2);
-    solTripleRecord_set_v3(r, v3);
-    solTriple_count_incr(t);
-    return 0;
-}
-
-int solTriple_resize(SolTriple *t, size_t s)
-{
-    SolTripleRecord *r = solTriple_records(t);
-    size_t *os = solTriple_size(t);
-    size_t l = SOL_TRIPLE_RESIZE_LIMIT;
-    solTriple_set_start_resizing_flag(t);
-    while (l-- && solTriple_is_resizing(t)) {
-        if (solTriple_set_size(t, s) != 0) {
-            solTriple_set_records(t, r);
-            // fix record node
-            solTriple_set_size(t, os);
-            solTriple_set_finish_resizing_flag(t);
-            return -1;
-        }
-        solTriple_set_count(t, 0);
-        if (solTriple_add_records(t, r, os) == 0) {
-            solTriple_set_finish_resizing_flag(t);
-        } else {
-            s = s * 2;
-            solTriple_free_records(solTriple_records(t), solTriple_size(t));
-        }
-    }
-    if (solTriple_is_resizing(t)) {
-        solTriple_set_records(t, r);
-        // fix record node
-        solTriple_set_size(t, os);
-        solTriple_set_finish_resizing_flag(t);
-        return -2;
-    }
-    solTriple_free_records(r, os);
-    return 0;
-}
-
-inline int solTriple_add_records(SolTriple *t, SolTripleRecord *records, size_t size)
-{
-    SolTripleRecord *r;
-    size_t offset = 0;
-    while(offset < size) {
-        r = solTriple_record_at_offset(records, offset);
-        if (solTripleRecord_v1(r)) {
-            if (solTriple_add(t, solTripleRecord_v1(r), solTripleRecord_v2(r), solTripleRecord_v3(r)) != 0) {
-                return -1;
-            }
-        }
-        offset++;
-    }
-    return 0;
-}
-
-inline SolHashRecord* solTriple_record1_of_val(SolTriple *t, void *v1, void *v2)
-{
-    size_t o1 = solTriple_v1_hash1(t, v1);
-    size_t o2 = solTriple_v2_hash1(t, v2);
-    o = (o1 & solTriple_mask(t) + o2 & solTriple_mask(t)) & solTriple_mask(t);
-    return solTriple_record_at_offset(solTriple_records(t), o);
-}
-
-inline SolHashRecord* solTriple_record2_of_val(SolTriple *t, void *v1, void *v2)
-{
-    size_t o1 = solTriple_v1_hash2(t, v1);
-    size_t o2 = solTriple_v2_hash2(t, v2);
-    o = (o1 & solTriple_mask(t) + o2 & solTriple_mask(t)) & solTriple_mask(t);
-    return solTriple_record_at_offset(solTriple_records(t), o);
+	if (t == NULL) {
+		return -1;
+	}
+	t->tmp_k->v1 = v1;
+	t->tmp_k->v2 = v2;
+	return solHash_get(t->h2, t->tml_k);
 }
--- src/basekit/sol_triple.h
+++ src/basekit/sol_triple.h
@@ -5,23 +5,15 @@
 #include "sol_hash.h"
 #include "sol_list.h"
 
-#define SOL_TRIPLE_INIT_SIZE 8
-#define SOL_TRIPLE_RESIZE_LIMIT 64
-
-typedef struct _SolTripleRecord {
-    void *v1;
-    void *v2;
-    void *v3;
-    void *n;
-} SolTripleRecord;
+typedef struct _SolTripleK {
+	void *v1;
+	void *v2;
+} SolTripleK;
 
 typedef struct _SolTriple {
-    size_t mask;
-    size_t size;
-    size_t count;
-    int flag;
-    SolTripleRecord *r;
-    SolHash *m;
+    SolHash *h1;
+	SolHash *h2;
+	SolTripleK *tmp_k;
     sol_f_hash_ptr f_v1_hash1;
     sol_f_hash_ptr f_v1_hash2;
     sol_f_hash_ptr f_v2_hash1;
@@ -32,50 +24,16 @@ typedef struct _SolTriple {
 
 SolTriple* solTriple_new();
 void solTriple_free(SolTriple*);
-int solTriple_add(SolTriple*, void*, void*, void*);
-void* solTriple_get_by_v1(SolTriple*, void*);
-void* solTriple_get_v3(SolTriple*, void*, void*);
+
+int solTriple_put(SolTriple*, void*, void*, void*);
+SolList* solTriple_get_by_v1(SolTriple*, void*);
+void* solTriple_get(SolTriple*, void*, void*);
+
 int solTriple_remove_by_v1(SolTriple*, void*);
-int solTriple_remove_v3(SolTriple*, void*, void*);
-int solTriple_set_size(SolTriple*, size_t);
-
-SolTripleRecord* solTripleRecord_new();
-void solTripleRecord_free();
-
-#define solTriple_set_records(t, r) t->record = r
-#define solTriple_set_size(t, s) t->size = s
-#define solTriple_set_count(t, c) t->count = c
-#define solTriple_update_mask(t) t->mask = t->size - 1
-
-#define solTriple_records(t) t->record
-#define solTriple_size(t) t->size
-#define solTriple_mask(t) t->mask
-#define solTriple_count(t) t->count
-
-#define solTriple_count_incr(t) t->count++
-
-#define solTripleRecord_set_v1(r, val) r->v1 = val
-#define solTripleRecord_set_v2(r, val) r->v2 = val
-#define solTripleRecord_set_v3(r, val) r->v3 = val
-#define solTripleRecord_set_next(r, val) r->n = val
+int solTriple_remove(SolTriple*, void*, void*);
 
 #define solTripleRecord_v1(r) r->v1
 #define solTripleRecord_v2(r) r->v2
-#define solTripleRecord_v3(r) r->v3
 #define solTripleRecord_next(r) r->n
-
-#define solTriple_v1_hash1(h) (*h->f_v1_hash1)
-#define solTriple_v1_hash2(h) (*h->f_v1_hash2)
-#define solTriple_v1_match(h) (*h->f_v1_match)
-
-#define solTriple_v2_hash1(h) (*h->f_v2_hash1)
-#define solTriple_v2_hash2(h) (*h->f_v2_hash2)
-#define solTriple_v2_match(h) (*h->f_v2_match)
-
-#define solTriple_record_at_offset(r, o) (SolTripleRecord*)(r + o)
-#define solTriple_grow(t) solTriple_resize(t, solTriple_size(t) *2)
-#define solTriple_is_resizing(t) t->flag & 0x1
-#define solTriple_set_start_resizing_flag(t) t->is_resizing | 0x1
-#define solTriple_set_finish_resizing_flag(t) t->is_resizing | 0x0
 
 #endif
