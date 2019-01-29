d828a09815e675c69d8250859c9902ed69c958b1
vincenttone <tongwensong@gmail.com>
2019-01-29 07:26:20 +0000

    bytecodes

---
+++ src/vm/bytecodes/sol_bytecodes.h
@@ -0,0 +1,73 @@
+#ifndef SOL_BYTECODES
+#define SOL_BYTECODES 1
+
+enum SolByteCodes {
+	_illegal = -1,
+	_nop     = 0,
+	
+	_move,
+
+	_load,
+
+	// _store,
+	_store,
+
+	_push,
+	_pop,
+	_dup,
+	_swap,
+
+	// _sadd, // short
+	_iadd, // int
+	_fadd, // float
+
+	// _ssub,
+	_isub,
+	_fsub,
+
+	_imul,
+	_fmul,
+
+	_idiv,
+	_fdiv,
+
+	_irem,
+	_frem,
+
+	_ineg,
+	_fneg,
+
+	_shl,
+
+	_shr,
+
+	_ushr,
+
+	_and,
+
+	_or,
+
+	_xor,
+
+	_iinc,
+
+	_ifeq,
+	_ifne,
+	_iflt,
+	_ifge,
+	_ifgt,
+	_ifle,
+
+	_goto,
+	_jsr,
+	_call,
+	_ret,
+
+	_trap,
+	_eret,
+
+
+	number_of_codes,
+}
+
+#endif
